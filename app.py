a87ea7812fcebe71375aa408efa3d63b4ff574cb
Arturo Aguilar Santos <106851100+Arturo210716@users.noreply.github.com>
2024-08-22 17:09:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+
+import os
 from fastapi import FastAPI
+import uvicorn
 #from routes.persona import persona
 #from routes.usuario import usuario
 from routes.users import user
@@ -6,6 +9,18 @@ from routes.persons import person
 from routes.roles import roles
 from routes.usersrols import userrol
 
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app= FastAPI()
 #app.include_router(persona)
@@ -16,7 +31,9 @@ app.include_router(roles)
 app.include_router(userrol)
 
 
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 print("Bienvenido a mi aplicacion")
