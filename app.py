41ddc0591ab976014f45cc29f111db0208724fa2
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-08-23 12:01:11 -0600

    Api actualizada

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-
-import os
 from fastapi import FastAPI
-import uvicorn
 #from routes.persona import persona
 #from routes.usuario import usuario
 from routes.users import user
 from routes.persons import person
 from routes.roles import roles
 from routes.usersrols import userrol
+from routes.receta import receta
+from routes.citas import cita
+from routes.expediente import expediente
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import uvicorn
 
-origins = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
+app = FastAPI()
 
+# Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Cambia "*" por una lista de orígenes permitidos
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los headers
 )
 
-app= FastAPI()
 #app.include_router(persona)
 #app.include_router(usuario)
 app.include_router(user)
 app.include_router(person)
 app.include_router(roles)
 app.include_router(userrol)
+app.include_router(receta)
+app.include_router(cita)
+app.include_router(expediente)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 
 
 print("Bienvenido a mi aplicacion")
